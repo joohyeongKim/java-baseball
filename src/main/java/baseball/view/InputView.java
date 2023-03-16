5329830e63cd450eb67c37f5bac7ec1d1650d055
김주형 <wnguddl96@knou.ac.kr>
2023-03-16 19:42:19 +0900

    refactor(InputView) : 게임 재시작 여부 내용 출력과 입력값을 받는 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,14 +1,31 @@
 package baseball.view;
 
+import baseball.controller.BaseBallGameStatus;
 import camp.nextstep.edu.missionutils.Console;
 
 public enum InputView {
     INSTANCE;
 
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String INPUT_GAME_STATUS = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    // 입력값을 출력하라
     public String inputBaseBallNumber() {
         System.out.println(INPUT_NUMBER);
-        return Console.readLine();
+        String baseBallNumber = Console.readLine();
+
+        InputValidator.validateBaseballNumber(baseBallNumber);
+
+        return baseBallNumber;
+    }
+
+    // 게임 재시작/종료 여부를 입력하라
+    public BaseBallGameStatus inputGameStatus() {
+        System.out.println(INPUT_GAME_STATUS);
+        String gameStatus = Console.readLine();
+
+        InputValidator.validateGameStatus(gameStatus);
+
+        return BaseBallGameStatus.getEnum(Integer.valueOf(gameStatus));
     }
 }
--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -2,18 +2,22 @@ package baseball.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import baseball.controller.BaseBallGameStatus;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("InputView 클래스")
+@SuppressWarnings({"InnerClassMayBestaticc", "NonAsciiCharacters"})
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class InputViewTest {
 
@@ -35,21 +39,90 @@ class InputViewTest {
         PRINT.reset();
     }
 
-    @Test
-    public void 숫자를_입력_받은_후_반환하라() throws Exception{
-        //given
-        String expectNumber = "123";
-        String expectString = "숫자를 입력해주세요 : \n";
-        InputStream inputStream = generateUserInput(expectNumber);
-        System.setIn(inputStream);
+    @Nested
+    class inputBaseBallNumber는_숫자_입력_문구를_출력 {
+        private String console = "숫자를 입력해주세요 : \n";
 
-        //when
-        String actualNumber = inputView.inputBaseBallNumber();
-        String actualString = PRINT.toString();
+        @Nested
+        class 만약_숫자를_입력받으면 {
+            final String GIVEN = "123";
+            private InputStream inputStream = generateUserInput(GIVEN);
 
-        //then
-        assertEquals(expectNumber, actualNumber);
-        assertEquals(expectString, actualString);
+            @Test
+            public void 그대로_리턴하라() {
+                //given
+                System.setIn(inputStream);
+                String actual = inputView.inputBaseBallNumber();
+
+                //when
+
+                //then
+                assertEquals(PRINT.toString(), console);
+                assertEquals(GIVEN, actual);
+            }
+        }
+
+        @Nested
+        class 만약_허용된_입력값이_아닌_경우 {
+            private InputStream inputStream = generateUserInput("999");
+
+            @Test
+            public void 예외_처리(){
+                //given
+                System.setIn(inputStream);
+
+                //then
+                Assertions.assertThatThrownBy(inputView::inputBaseBallNumber)
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        class inputGameStatus는_게임_재시작_여부_문구_출력 {
+            private String console = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+            @Nested
+            class 만약_1을_입력받는_경우 {
+                String GIVEN = "1";
+                private InputStream given = generateUserInput(GIVEN);
+
+                @Test
+                public void BaseBallGameStatus_Play를_리턴() throws Exception{
+                    //given
+                    System.setIn(given);
+                    BaseBallGameStatus actualStatus = BaseBallGameStatus.PLAY;
+
+                    //when
+                    BaseBallGameStatus baseBallGameStatus = inputView.inputGameStatus();
+
+                    //then
+                    assertEquals(PRINT.toString(), console);
+                    assertEquals(baseBallGameStatus, actualStatus);
+                }
+            }
+
+            @Nested
+            class 만약_2를_입력받는_경우 {
+                private String GIVEN = "2";
+                private InputStream inputStream = generateUserInput(GIVEN);
+
+                @Test
+                public void BaseBallGameStatus_STOP을_리턴(){
+                    //given
+                    System.setIn(inputStream);
+
+                    //when
+                    BaseBallGameStatus baseBallGameStatus = inputView.inputGameStatus();
+                    BaseBallGameStatus actualStatus = BaseBallGameStatus.STOP;
+
+                    //then
+                    assertEquals(PRINT.toString(), console);
+                    assertEquals(baseBallGameStatus, actualStatus);
+                }
+            }
+        }
+
     }
+
 
 }
