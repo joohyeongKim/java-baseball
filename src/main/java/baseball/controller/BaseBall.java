61a63dc86f31a192ed4494c66de0f5eece959f85
김주형 <wnguddl96@knou.ac.kr>
2023-03-16 17:19:30 +0900

    refactor(BaseBall) : 객체 동등성 비교를 위한 읿반 규약 equals() 재정의와 hashCode() 재정의

--- src/main/java/baseball/controller/BaseBall.java
+++ src/main/java/baseball/controller/BaseBall.java
@@ -1,5 +1,7 @@
 package baseball.controller;
 
+import java.util.Objects;
+
 public class BaseBall {
 
     private Integer number;
@@ -10,5 +12,25 @@ public class BaseBall {
 
     public BaseBall(Integer number) {
         this.number = number;
+    }
+
+    // equals를 재정의 하려거든 hashCode도 재정의하라
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        BaseBall baseBall = (BaseBall) obj;
+        return Objects.equals(number, baseBall.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
