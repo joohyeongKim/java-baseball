14659a6095240601e0124497d4e4dd89be2906b1
김주형 <wnguddl96@knou.ac.kr>
2023-03-16 16:59:45 +0900

    feat(BaseBalls) : 3자리 숫자 리스트를 관리하는 일급 컬렉션 추가

---
+++ src/main/java/baseball/controller/BaseBalls.java
@@ -0,0 +1,12 @@
+package baseball.controller;
+
+import java.util.List;
+
+public class BaseBalls {
+
+    private List<BaseBall> baseBallNumbers;
+
+    public BaseBalls(List<BaseBall> baseBallNumbers) {
+        this.baseBallNumbers = baseBallNumbers;
+    }
+}
