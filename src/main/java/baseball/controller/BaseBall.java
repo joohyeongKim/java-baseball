9c67ef988588cd6c7b5a010b81dadb03245c3f05
김주형 <wnguddl96@knou.ac.kr>
2023-03-16 16:56:45 +0900

    feat(BaseBall) : 각 숫자를 표현하는 책임 추가

---
+++ src/main/java/baseball/controller/BaseBall.java
@@ -0,0 +1,14 @@
+package baseball.controller;
+
+public class BaseBall {
+
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public BaseBall(Integer number) {
+        this.number = number;
+    }
+}
